4090f799a15648e6be29871b0f11e6543d816f1e
Scott M. Silver <scott.m.silver@alum.dartmouth.org>
2017-01-23 21:32:34 -0800

    Add Google unit tests (but no real tests).

--- src/wordescape.cpp
+++ src/wordescape.cpp
@@ -86,7 +86,7 @@ struct WordBaseMove : public Move<WordBaseMove> {
 };
 
 
-// Return string with spaces roved from end of supplied string.
+// Return string with spaces removed from end of supplied string.
 static inline std::string &rtrim(std::string &s) {
   s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
   return s;
